1e58290d6a272b4f436978b4b732425c0864e621
Chad Haselwander <chaselwand@healthgrades.com>
2021-07-12 17:00:36 -0500

    fixed baseURL value and added headers property

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https:api.themoviedb.org/3"
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    "Content-Type": "application/json;charset=UTF-8",
+  },
 });
 
 export default instance;
